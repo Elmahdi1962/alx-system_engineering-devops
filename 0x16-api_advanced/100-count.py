4de16cb8b9da05599c01f93544e4808d17217a7a
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-15 21:05:56 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -25,12 +25,11 @@ def count_words(subreddit, word_list, found_list=[], after=None):
             count_words(subreddit, word_list, found_list, aft)
         else:
             result = {}
-            for word in found_list:
+            for word in found_list.sort():
                 if word.lower() in result.keys():
                     result[word.lower()] += 1
                 else:
                     result[word.lower()] = 1
-            result = sorted(result)
             for key, value in sorted(result.items(), key=lambda item: item[1],
                                      reverse=True):
                 print('{}: {}'.format(key, value))
