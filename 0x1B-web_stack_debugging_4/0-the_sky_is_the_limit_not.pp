19ca5aa0a5dec4defed560bad818ebb3ac3cc360
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-02-24 13:28:29 +0100

    undo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # change the limit ofopen files in nginx config
-
 exec {'change limit':
-  command  => "ulimit -Hn #hard 4096 && ulimit -Sn #soft 4096",
+  command  => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
   provider => shell
 }
