2bc0e7f512ea8aad69426cf5a93d9d3b6af9246d
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-24 13:14:12 +0100

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # change the limit ofopen files in nginx config
+
 exec {'change limit':
-  command  => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
+  command  => "ulimit -Hn #hard 4096 && ulimit -Sn #soft 4096",
   provider => shell
 }
